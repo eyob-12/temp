cf517d4ae46eb122f32e316aa3e58606e9b06405
turbo2121 <eyobaddis394@gmail.com>
2024-06-01 20:12:13 +0300

    update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const baseURL = import.meta.env.VITE_API_URL || "http://localhost:4000/api/";
+
 export const client = axios.create({
-	baseURL: "http://localhost:4000/api/",
+	baseURL,
 });
