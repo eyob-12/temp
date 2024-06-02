559889155f1230757642e2ebe851e5d2bb5d2123
turbo2121 <eyobaddis394@gmail.com>
2024-06-02 13:59:12 +0300

    update

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:4000/api/";
+const baseURL = "https://abyssiniasounds.vercel.app/api/";
 
 export const client = axios.create({
 	baseURL,
